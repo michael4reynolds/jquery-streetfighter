599d471bbc8149aeda3955487874700049fb47e2
Michael Reynolds <michael4reynolds@gmail.com>
2016-04-22 13:51:01 -0400

    prepare for hadouken

--- js/app.js
+++ js/app.js
@@ -3,8 +3,17 @@ $(document).ready(function () {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
     })
-    .mouseleave(function() {
-        $('.ryu-ready').hide();
-        $('.ryu-still').show();
-    })
+        .mouseleave(function () {
+            $('.ryu-ready').hide();
+            $('.ryu-still').show();
+        })
+        .mousedown(function () {
+            console.log('mousedown');
+            // play hadouken sound
+            // show hadouken and animate it to the right of the screen
+        })
+        .mouseup(function () {
+            console.log('mouseup');
+            // ryu goes back to his ready position
+        })
 });
