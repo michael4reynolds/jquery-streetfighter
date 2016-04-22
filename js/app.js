888a1f4511293dab0de3696ab7b5e0220f0a2573
Michael Reynolds <michael4reynolds@gmail.com>
2016-04-22 13:42:33 -0400

    ryu ready 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $('.ryu').mouseenter(function () {
-        alert('mouse entered .ryu div')
-    });
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-ready').hide();
+        $('.ryu-still').show();
+    })
 });
