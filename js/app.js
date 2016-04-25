591a864f7cd3e80498378fe1e79d196bb6be2b75
Michael Reynolds <michael4reynolds@gmail.com>
2016-04-25 11:40:33 -0400

    begin create Hulk Ryu

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     $('.ryu')
         .mouseenter(function () {
-            $('.ryu-still').hide();
-            $('.ryu-ready').show();
+            $('.ryu .ryu-still').hide();
+            $('.ryu .ryu-ready').show();
         })
         .mouseleave(function () {
-            $('.ryu-ready').hide();
-            $('.ryu-still').show();
+            $('.ryu .ryu-ready').hide();
+            $('.ryu .ryu-still').show();
         })
         .mousedown(function () {
             playHadouken();
-            $('.ryu-ready').hide();
-            $('.ryu-cool').hide();
-            $('.ryu-still').hide();
-            $('.ryu-throwing').show();
+            $('.ryu .ryu-ready').hide();
+            $('.ryu .ryu-cool').hide();
+            $('.ryu .ryu-still').hide();
+            $('.ryu .ryu-throwing').show();
             $('.hadouken').finish().show().animate(
                 { 'left': '1020px' },
                 500,
@@ -24,21 +24,59 @@ $(document).ready(function () {
             );
         })
         .mouseup(function () {
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
+            $('.ryu .ryu-throwing').hide();
+            $('.ryu .ryu-ready').show();
+        });
+    $('.hulk-ryu')
+        .mouseenter(function () {
+            $('.hulk-ryu .ryu-still').hide();
+            $('.hulk-ryu .ryu-ready').show();
+        })
+        .mouseleave(function () {
+            $('.hulk-ryu .ryu-ready').hide();
+            $('.hulk-ryu .ryu-still').show();
+        })
+        .mousedown(function () {
+            playHadouken();
+            $('.hulk-ryu .ryu-ready').hide();
+            $('.hulk-ryu .ryu-cool').hide();
+            $('.hulk-ryu .ryu-still').hide();
+            $('.hulk-ryu .ryu-throwing').show();
+            $('.hulk-hadouken').finish().show().animate(
+                { 'left': '1020px' },
+                500,
+                function () {
+                    $(this).hide();
+                    $(this).css('left', '520px');
+                }
+            );
+        })
+        .mouseup(function () {
+            $('.hulk-ryu .ryu-throwing').hide();
+            $('.hulk-ryu .ryu-ready').show();
         });
     $('body').keydown(function (e) {
         if (e.which == 88) {
-            $('.ryu-ready').hide();
-            $('.ryu-still').hide();
-            $('.ryu-cool').show();
+            $('.ryu .ryu-ready').hide();
+            $('.ryu .ryu-still').hide();
+            $('.ryu .ryu-cool').show();
+        }
+        if (e.which == 89) {
+            $('.hulk-ryu .ryu-ready').hide();
+            $('.hulk-ryu .ryu-still').hide();
+            $('.hulk-ryu .ryu-cool').show();
         }
     });
     $('body').keyup(function (e) {
         if (e.which == 88) {
-            $('.ryu-ready').hide();
-            $('.ryu-cool').hide();
-            $('.ryu-still').show();
+            $('.ryu .ryu-ready').hide();
+            $('.ryu .ryu-cool').hide();
+            $('.ryu .ryu-still').show();
+        }
+        if (e.which == 89) {
+            $('.hulk-ryu .ryu-ready').hide();
+            $('.hulk-ryu .ryu-cool').hide();
+            $('.hulk-ryu .ryu-still').show();
         }
     })
 });
